4c563619eb25e966f7136bb912b160327e82d4f6
jackarthurm <jackarthurm@gmail.com>
2019-03-17 19:32:25 +0000

    Implemented Facebook share link component

---
+++ src/app/components/fb-share-link/fb-share-link.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FbShareLinkComponent } from './fb-share-link.component';
+
+describe('FbShareLinkComponent', () => {
+  let component: FbShareLinkComponent;
+  let fixture: ComponentFixture<FbShareLinkComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ FbShareLinkComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(FbShareLinkComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/fb-share-link/fb-share-link.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { environment } from '@envs/environment';
+
+@Component({
+  selector: 'gal-fb-share-link',
+  templateUrl: './fb-share-link.component.html',
+  styleUrls: ['./fb-share-link.component.css']
+})
+export class FbShareLinkComponent {
+
+  fbShareLink: string = `https://www.facebook.com/sharer/sharer.php?u=${environment.siteUrl.schema}%3A%2F%2F${environment.siteUrl.domain}%2F&amp;src=sdkpreparse`
+  fbDataHref: string = `${environment.siteUrl.schema}://${environment.siteUrl.domain}`
+}
