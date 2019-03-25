871eeae2e8bf5b272b7052b9c24eb89c8006c24e
jackarthurm <jackarthurm@gmail.com>
2019-03-25 22:11:50 +0000

    Refactored image service to use real API

--- src/app/services/image/image.service.ts
+++ src/app/services/image/image.service.ts
@@ -1,34 +1,93 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
-import { uuid } from '@app/models/shared';
+import { uuid, url } from '@app/models/shared';
 import { environment } from '@envs/environment'
+import { map } from 'rxjs/operators';
 
 
-export enum ImageSizeType {
-  THUMBNAIL = 'thumbnail',
-  MEDIUM = 'medium',
-  FULL = 'full'
+// Model definitions
+export interface ImageFile {
+  location: url;
+  height: number;
+  width: number;
 }
-
 
 export interface ImageProperties {
-  imageID: uuid;
-  name: string;
-  width: number;
-  height: number;
-  caption: string;
+  id: uuid;
+  originalImage: ImageFile;
+  reducedImage: ImageFile;
+  title: string;
   description: string;
-  media: string;
-  tags: Array<string>;
-  artistName: string;
-  sizeType: ImageSizeType;
+  media_description: string;
+  artist_name: string;
+  // tags: Array<url>  # TODO
 }
-
 
 export interface GalleryPage {
   pageIndex: number;
+  itemCount: number;
+  nextPage: url | null;
+  previousPage: url | null;
   images: Array<ImageProperties>
+}
+
+// API schema definition
+interface GalleryItemListAPIResult {
+  count: number;
+  next: url | null;
+  previous: url | null;
+  results: Array<GalleryItemAPIResult>;
+}
+
+interface GalleryItemAPIResult {
+  id: uuid;
+  original_image: url;
+  original_width: number;
+  original_height: number;
+  reduced_image: url;
+  reduced_width: number;
+  reduced_height: number;
+  title: string;
+  description: string;
+  media_description: string;
+  artist_name: string;
+  tags: Array<url>;
+}
+
+// Mapping layer
+function imageResult(res: GalleryItemAPIResult): ImageProperties {
+  return {
+    id: res.id,
+    originalImage: {
+      location: res.original_image,
+      height: res.original_height,
+      width: res.original_width
+    },
+    reducedImage: {
+      location: res.reduced_image,
+      height: res.reduced_height,
+      width: res.reduced_width
+    },
+    title: res.title,
+    description: res.description,
+    media_description: res.media_description,
+    artist_name: res.artist_name
+  };
+}
+
+function imagePageResult(
+  res: GalleryItemListAPIResult, 
+  pageIndex: number
+): GalleryPage {
+
+  return {
+    pageIndex: pageIndex,
+    itemCount: res.count,
+    nextPage: res.next,
+    previousPage: res.previous,
+    images: res.results.map(imageResult)
+  }
 }
 
 
@@ -39,16 +98,15 @@ export class ImageService {
 
   constructor(private _http: HttpClient) {}
 
-  getImage(imageID: uuid): Observable<Blob> {
-    /* Return a placeholder image in blob format */
+  getImage(location: url): Observable<Blob> {
+    /* Retrieve an image file in blob format */
 
     const headers: HttpHeaders = new HttpHeaders({
       'Accept': 'application/octet-stream'
     });
 
     return this._http.get<Blob>(
-      `${environment.imagesURL.schema}://
-${environment.imagesURL.domain}${environment.imagesURL.ext}?image=` + imageID,
+      location,
       {
         headers: headers,
         responseType: 'blob' as 'json'
@@ -56,195 +114,51 @@ ${environment.imagesURL.domain}${environment.imagesURL.ext}?image=` + imageID,
     )
   }
 
-  getGalleryPage(pageIndex: number): Observable<GalleryPage> {
+  getGalleryItem(imageID: uuid): Observable<ImageProperties> {
 
-    return of(
+    const headers: HttpHeaders = new HttpHeaders({
+      'Accept': 'application/json'
+    });
+
+    return this._http.get<GalleryItemAPIResult>(
+      `http://localhost:8001/gallery-items/${imageID}/`,
       {
-        pageIndex: pageIndex,
-        images: [
-          {
-            imageID: '10',
-            name: 'test name 1',
-            width: 300,
-            height: 400,
-            caption: 'test caption',
-            description: 'test description',
-            media: 'test media',
-            tags: [
-              'tag1',
-              'tag2'
-            ],
-            artistName: 'Kate Alice Mann',
-            sizeType: ImageSizeType.MEDIUM
-          },
-          {
-            imageID: '20',
-            name: 'test name 2',
-            width: 300,
-            height: 400,
-            caption: 'test caption',
-            description: 'test description',
-            media: 'test media',
-            tags: [
-              'tag1',
-              'tag2'
-            ],
-            artistName: 'Kate Alice Mann',
-            sizeType: ImageSizeType.MEDIUM
-          },
-          {
-            imageID: '30',
-            name: 'test name 3',
-            width: 300,
-            height: 400,
-            caption: 'test caption',
-            description: 'test description',
-            media: 'test media',
-            tags: [
-              'tag1',
-              'tag2'
-            ],
-            artistName: 'Kate Alice Mann',
-            sizeType: ImageSizeType.MEDIUM
-          },
-          {
-            imageID: '40',
-            name: 'test name 4',
-            width: 300,
-            height: 400,
-            caption: 'test caption',
-            description: 'test description',
-            media: 'test media',
-            tags: [
-              'tag1',
-              'tag2'
-            ],
-            artistName: 'Kate Alice Mann',
-            sizeType: ImageSizeType.MEDIUM
-          },
-          {
-            imageID: '50',
-            name: 'test name 5',
-            width: 300,
-            height: 400,
-            caption: 'test caption',
-            description: 'test description',
-            media: 'test media',
-            tags: [
-              'tag1',
-              'tag2'
-            ],
-            artistName: 'Kate Alice Mann',
-            sizeType: ImageSizeType.MEDIUM
-          },
-          {
-            imageID: '60',
-            name: 'test name 6',
-            width: 300,
-            height: 400,
-            caption: 'test caption',
-            description: 'test description',
-            media: 'test media',
-            tags: [
-              'tag1',
-              'tag2'
-            ],
-            artistName: 'Kate Alice Mann',
-            sizeType: ImageSizeType.MEDIUM
-          },
-          {
-            imageID: '70',
-            name: 'test name 7',
-            width: 300,
-            height: 400,
-            caption: 'test caption',
-            description: 'test description',
-            media: 'test media',
-            tags: [
-              'tag1',
-              'tag2'
-            ],
-            artistName: 'Kate Alice Mann',
-            sizeType: ImageSizeType.MEDIUM
-          },
-          {
-            imageID: '80',
-            name: 'test name 8',
-            width: 300,
-            height: 400,
-            caption: 'test caption',
-            description: 'test description',
-            media: 'test media',
-            tags: [
-              'tag1',
-              'tag2'
-            ],
-            artistName: 'Kate Alice Mann',
-            sizeType: ImageSizeType.MEDIUM,
-            slug: 'a-specific-picture'
-          },
-          {
-            imageID: '90',
-            name: 'test name 9',
-            width: 300,
-            height: 400,
-            caption: 'test caption',
-            description: 'test description',
-            media: 'test media',
-            tags: [
-              'tag1',
-              'tag2'
-            ],
-            artistName: 'Kate Alice Mann',
-            sizeType: ImageSizeType.MEDIUM
-          },
-          {
-            imageID: '100',
-            name: 'test name 10',
-            width: 300,
-            height: 400,
-            caption: 'test caption',
-            description: 'test description',
-            media: 'test media',
-            tags: [
-              'tag1',
-              'tag2'
-            ],
-            artistName: 'Kate Alice Mann',
-            sizeType: ImageSizeType.MEDIUM
-          },
-          {
-            imageID: '110',
-            name: 'test name 11',
-            width: 300,
-            height: 400,
-            caption: 'test caption',
-            description: 'test description',
-            media: 'test media',
-            tags: [
-              'tag1',
-              'tag2'
-            ],
-            artistName: 'Kate Alice Mann',
-            sizeType: ImageSizeType.MEDIUM
-          },
-          {
-            imageID: '120',
-            name: 'test name 12',
-            width: 300,
-            height: 400,
-            caption: 'test caption',
-            description: 'test description',
-            media: 'test media',
-            tags: [
-              'tag1',
-              'tag2'
-            ],
-            artistName: 'Kate Alice Mann',
-            sizeType: ImageSizeType.MEDIUM
-          }
-        ]
+        headers: headers,
+        responseType: 'json'
       }
-    )
+    ).pipe(
+      map(
+        (res: GalleryItemAPIResult) => imageResult(res)
+      )
+    );
+  }
+
+  getGalleryPage(
+    pageIndex: number = 1, 
+    pageSize: number = 12
+  ): Observable<GalleryPage> {
+
+    const headers: HttpHeaders = new HttpHeaders({
+      'Accept': 'application/octet-stream'
+    });
+
+    return this._http.get<GalleryItemListAPIResult>(
+      `${environment.imagesURL.schema}://${environment.imagesURL.domain}
+      ${environment.imagesURL.ext}`,
+      {
+        headers: headers,
+        responseType: 'json',
+        params: {
+          page: pageIndex.toString(), 
+          size: pageSize.toString()
+        }
+      }
+    ).pipe(
+      map(
+        (page: GalleryItemListAPIResult) => imagePageResult(page, pageIndex)
+      )
+    );
   }
 }
+
+
