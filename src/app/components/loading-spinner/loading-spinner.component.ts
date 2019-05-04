c610af4a0550b7bb07275fc23fb543e8a8f9294d
Jack Mann <jackarthurm@gmail.com>
2019-05-04 17:09:18 +0100

    IMplemented loading spinner component

--- src/app/components/loading-spinner/loading-spinner.component.ts
+++ src/app/components/loading-spinner/loading-spinner.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 
 @Component({
@@ -7,4 +7,3 @@ import { Component } from '@angular/core';
   styleUrls: ['./loading-spinner.component.scss']
 })
 export class LoadingSpinnerComponent {}
-
